44f6053d94e03bd1437ede9a960bb72552a302c4
Mwenya <mwen@hotmail.co.uk>
2024-05-17 13:15:15 +0100

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer') /*  middleware for handling multipart/form-data, which is primarily used for uploading files */
+const upload = multer({dest: 'uploads/'})
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',
+  upload.single('upfile'), /* input file from webform */
+  (req, res, next) => {
+    return res.status(201).json({ /* return test object */
+      'name': req.file.originalname,
+      'type': req.file.mimetype,
+      'size': req.file.size
+    })
+  })
 
 
 const port = process.env.PORT || 3000;
